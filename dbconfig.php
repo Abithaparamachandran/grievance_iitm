edc5371bb5d4e38038c718f7c8ad0080536d5d20
abi <165017113+Abithaparamachandran@users.noreply.github.com>
2025-07-03 16:14:49 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,18 +1,12 @@
 <?php
   
-$servername = "localhost";
-
-$username = "root";
-
-$password = "Password1";
-$dbname = "grievance";
-
-
+$servername = "**";
+$username = "***";
+$password = "***";
+$dbname = "***";
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
-
-    die("Connection failed: " . $conn->connect_error);
+   die("Connection failed: " . $conn->connect_error);
 
 }
 
