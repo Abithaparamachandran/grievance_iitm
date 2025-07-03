61b3593891487ff7d561b7730cdefdea00b02ef8
abi <165017113+Abithaparamachandran@users.noreply.github.com>
2025-07-03 16:14:01 +0530

    Update export.php

--- export.php
+++ export.php
@@ -5,7 +5,7 @@
     header('Content-Disposition: attachment; filename=data.csv');
     $output = fopen("php://output", "w");
     fputcsv($output, array('ID','Name', 'Subject','Message','File','Created Date'));
-    $con=mysqli_connect("localhost","root","r00t@123","liveportal");
+    $con=mysqli_connect("***","***","***","***");
     $query = "SELECT id,name,subject,msg,file,createddate from grievanceredressalusers";
     $result = mysqli_query($con, $query);
     while($row = mysqli_fetch_assoc($result))
